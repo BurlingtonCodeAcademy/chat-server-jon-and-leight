8b0f04b80485b7f4d7b1106f13df892ae0ffcd5f
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-11-15 06:46:25 -0500

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const $path = require('path');
 const express = require('express');
+const Room = require('./lib/Room');
 
 const app = express();
 const port = process.env.PORT || 5000;
 const publicDir = $path.resolve('./public');
 
+app.get("/", (request, response) => {
+  const main = new Room('main')
+  response.sendFile($path.join(publicDir, "index.html"))
+})
+
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Chat app listening on port ${port}!`));
+

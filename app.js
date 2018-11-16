8447a39701c543880224a197d65be69455932fa1
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-11-15 19:25:08 -0500

    main chat working with author, started work on final class config

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const fs = require('fs');
 const $path = require('path');
 const express = require('express');
-const Room = require('./lib/room.js');
+const Room = require('./lib/room');
+const House = require('./lib/house');
+const Message = require('./lib/message');
 
 const app = express();
 const port = process.env.PORT || 5000;
 const publicDir = $path.resolve('./public');
 
-app.get("/", (request, response) => {
-  const main = new Room('main');
-  console.log(main);
+const house = new House();
+house.createRoom("main");
+
+
+
+app.get("/.json", (_request, response) => {
+  const messages = house.getRoom("main").getMessages()
+  response.send(messages)
+})
+
+app.get("/", (_request, response) => {
   response.sendFile($path.join(publicDir, "index.html"))
+})
+
+app.get("/chat/:room.json", (request, response) => {
+  const messages = house.getRoom(request.params.room).getMessages()
+  response.send(messages);
+})
+
+app.get("/chat/:room", (_request, response) => {
+  request.params.room
+  response.sendFile($path.join(publicDir, "index.html"))
+})
+
+app.post("/createRoom", express.urlencoded({extended: false}), (response, request) => {
+  house.createRoom(request.body.room)
+  response.redirect(`/chat/${request.body.room}`)
+})
+
+app.post("/send", express.urlencoded({ extended: false }), (request, response) => {
+  house.getRoom("main").createMessage(request.body.body, request.body.author)
+  response.redirect("/")
 })
 
 app.use(express.static('public'));
